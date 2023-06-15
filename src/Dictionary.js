037f6bd7e59daeef7109ca2dc7815473b6c6bfb7
misskatiemarple <misskatiemarple@gmail.com>
2023-06-15 19:55:33 +0900

    Integrated API and axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}`);
+
+    let apiKey = "3041fdbb74ta3a686b2ca3f782407o93";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
+  function handleResponse(response) {}
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
