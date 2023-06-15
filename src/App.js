e2201cbc0643b183bb622913c26529f1accf5d7d
misskatiemarple <misskatiemarple@gmail.com>
2023-06-15 18:44:04 +0900

    Added button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">hello</header>
+      <header className="App-header">
+        <a href="3" className="bt btn-primary"></a>
+      </header>
     </div>
   );
 }
